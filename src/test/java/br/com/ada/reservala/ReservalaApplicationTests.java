8fd2caf2529b49a39448f40fc0bb1d055e86a373
Joao Pedro de Faria Ferreira <joao.pedro.ferreira.profissional@gmail.com>
2024-05-31 11:09:03 -0300

    commit - mudanças pom.xml adição de dependencia RestAssured

--- src/test/java/br/com/ada/reservala/ReservalaApplicationTests.java
+++ src/test/java/br/com/ada/reservala/ReservalaApplicationTests.java
@@ -3,11 +3,19 @@ package br.com.ada.reservala;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
 @SpringBootTest
 class ReservalaApplicationTests {
 
 	@Test
 	void contextLoads() {
+		given()
+				.when().get("http://localhost:8080/room")
+				.then()
+				.body("roomNumber", is(hasItem(7)));
 	}
 
 }
