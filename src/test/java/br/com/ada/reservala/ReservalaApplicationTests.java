b5ecfa1158018fa1a9f69d8ecd9cdfe6ed0b5557
Joao Pedro de Faria Ferreira <joao.pedro.ferreira.profissional@gmail.com>
2024-06-02 18:10:21 -0300

    Commit - criaçao dos testes, validação API Get

--- src/test/java/br/com/ada/reservala/ReservalaApplicationTests.java
+++ src/test/java/br/com/ada/reservala/ReservalaApplicationTests.java
@@ -4,18 +4,57 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalToObject;
+import static org.hamcrest.Matchers.is;
 
 
 @SpringBootTest
 class ReservalaApplicationTests {
 
+
+
 	@Test
-	void contextLoads() {
+	void numberRoomValidation() {
+		int roomNumber = 0;
+
 		given()
 				.when().get("http://localhost:8080/room")
 				.then()
-				.body("roomNumber", is(hasItem(7)));
+				.body("roomNumber", is(equalToObject(roomNumber)))
+				.assertThat().statusCode(200);
+	}
+
+	@Test
+	void typeValidation() {
+		String roomtype = "Solteiro";
+
+		given()
+				.when().get("http://localhost:8080/room")
+				.then()
+				.body("type", is(equalToObject(roomtype)))
+				.assertThat().statusCode(200);
+	}
+
+	@Test
+	void priceValidation() {
+		int  roomPrice = 500;
+
+		given()
+				.when().get("http://localhost:8080/room")
+				.then()
+				.body("price", is(equalToObject(roomPrice)))
+				.assertThat().statusCode(200);
+	}
+
+	@Test
+	void avaliableValidation() {
+		boolean  roomAvaliable = true;
+
+		given()
+				.when().get("http://localhost:8080/room")
+				.then()
+				.body("avaliable", is(equalToObject(roomAvaliable)))
+				.assertThat().statusCode(200);
 	}
 
 }
