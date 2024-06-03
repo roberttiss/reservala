2d9453fa88973f62d6a627cd192e4543ae163810
roberts <gabrielrobertssoares.com>
2024-06-03 11:59:43 -0300

    criação de classe para tratar exception com o status personalizado de resposta ao erro not found

---
+++ src/main/java/br/com/ada/reservala/exception/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package br.com.ada.reservala.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RoomNotFoundException.class)
+    public ResponseEntity<String> handleRoomNotFoundException(RoomNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
---
+++ src/main/java/br/com/ada/reservala/exception/RoomNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.ada.reservala.exception;
+
+public class RoomNotFoundException extends RuntimeException{
+
+    public RoomNotFoundException(String message){
+        super(message);
+    }
+}
