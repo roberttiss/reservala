1170d34fff663bc1588bf1e8e7c49b57cc867341
roberts <gabrielrobertssoares.com>
2024-06-03 11:57:56 -0300

    criação do mapper

---
+++ src/main/java/br/com/ada/reservala/controller/mapper/RoomMapper.java
@@ -0,0 +1,37 @@
+package br.com.ada.reservala.controller.mapper;
+
+import br.com.ada.reservala.controller.dto.RoomDTORequest;
+import br.com.ada.reservala.controller.dto.RoomDTOResponse;
+import br.com.ada.reservala.domain.Room;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class RoomMapper {
+
+    public Room toEntity(final RoomDTORequest dto){
+        return Room.builder()
+                .roomNumber(dto.getRoomNumber())
+                .type(dto.getType())
+                .price(dto.getPrice())
+                .avalaible(dto.getAvalaible())
+                .build();
+    }
+
+    public List<RoomDTOResponse> toDto(final List<Room> entities){
+        return entities.stream()
+                .map(this::toDto)
+                .collect(Collectors.toList());
+    }
+
+    public RoomDTOResponse toDto(final Room entity){
+        return RoomDTOResponse.builder()
+                .roomNumber(entity.getRoomNumber())
+                .type(entity.getType())
+                .price(entity.getPrice())
+                .avalaible(entity.getAvalaible())
+                .build();
+    }
+}
