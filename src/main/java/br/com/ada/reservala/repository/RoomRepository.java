315b336f55ace8117dd9d7308954e211e1ef3d9c
roberts <gabrielrobertssoares.com>
2024-06-03 12:08:44 -0300

    criação de querys para calculo de ocupação, receita e existencia de room dado tal roomNumber

--- src/main/java/br/com/ada/reservala/repository/RoomRepository.java
+++ src/main/java/br/com/ada/reservala/repository/RoomRepository.java
@@ -12,10 +12,10 @@ public class RoomRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
-    private String createSQL = "insert into room(roomNumber, type, price, avalaible) values (?, ?, ?, ?)";
+    private String createSQL = "insert into room(roomNumber,type, price, avalaible) values (?, ?, ?, ?)";
     private String readSQL = "select * from room";
-    private String updateSQL = "update room ";
-    private String deleteSQL = "delete from room";
+    private String updateSQL = "update room set type = ?, price = ?, avalaible = ? where roomNumber = ? ";
+    private String deleteSQL = "delete from room where roomNumber = ? ";
 
     public RoomRepository(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -27,7 +27,7 @@ public class RoomRepository {
                 room.getRoomNumber(),
                 room.getType(),
                 room.getPrice(),
-                room.getAvailable()
+                room.getAvalaible()
         );
         return room;
     }
@@ -42,13 +42,35 @@ public class RoomRepository {
         return jdbcTemplate.query(readSQL, rowMapper);
     }
 
-    public Room updateRoom(Room room){
-        jdbcTemplate.update(updateSQL, room);
+    public Room updateRoom(Room room, Integer roomNumber){
+        jdbcTemplate.update(
+                updateSQL,
+                room.getType(),
+                room.getPrice(),
+                room.getAvalaible(),
+                roomNumber
+        );
         return room;
     }
 
     public void deleteRoom(Integer roomNumber){
         jdbcTemplate.update(deleteSQL, roomNumber);
+    }
+
+    public Double count(){
+        return jdbcTemplate.queryForObject("select count(*) from room", Double.class);
+    }
+
+    public Double countOccupied(){
+        return jdbcTemplate.queryForObject("select count(*) from room where avalaible = false", Double.class);
+    }
+
+    public Double countRevenue(){
+        return jdbcTemplate.queryForObject("select sum(price) from room", Double.class);
+    }
+
+    public Integer existsRoomNumber(Integer roomNumber){
+        return jdbcTemplate.queryForObject("select count(*) from room where roomNumber = ?", Integer.class,roomNumber);
     }
 
 }
