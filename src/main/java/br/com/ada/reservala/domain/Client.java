baaf5adb01ceba8047497e78d2075bd1ff02cbc3
roberts <gabrielrobertssoares.com>
2024-06-09 16:21:46 -0300

    criação client

---
+++ src/main/java/br/com/ada/reservala/domain/Client.java
@@ -0,0 +1,26 @@
+package br.com.ada.reservala.domain;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class Client {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int idClient;
+    @NotBlank
+    private String name;
+    @NotNull
+    private int age;
+//    @NotNull
+//    private int yearBirth;
+//    @OneToMany(mappedBy = "client")
+//    private List<Reservation> reservations;
+}
