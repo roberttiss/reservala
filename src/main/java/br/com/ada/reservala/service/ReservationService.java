4e237ef002cfec7357c8e10ec8e957eff2d99331
roberts <gabrielrobertssoares.com>
2024-06-11 16:26:49 -0300

    validações de metodos

--- src/main/java/br/com/ada/reservala/service/ReservationService.java
+++ src/main/java/br/com/ada/reservala/service/ReservationService.java
@@ -3,6 +3,7 @@ package br.com.ada.reservala.service;
 import br.com.ada.reservala.domain.Reservation;
 import br.com.ada.reservala.exception.ClientNotFoundException;
 import br.com.ada.reservala.exception.ReservationNotFoundException;
+import br.com.ada.reservala.exception.RoomNotFoundException;
 import br.com.ada.reservala.repository.ClientRepository;
 import br.com.ada.reservala.repository.ReservationRepository;
 import br.com.ada.reservala.repository.RoomRepository;
@@ -19,11 +20,20 @@ public class ReservationService {
     private RoomRepository roomRepository;
     private ClientRepository clientRepository;
 
-    public ReservationService(ReservationRepository repository) {
+    public ReservationService(ReservationRepository repository, ClientRepository clientRepository, RoomRepository roomRepository) {
         this.reservationRepository = repository;
+        this.clientRepository = clientRepository;
+        this.roomRepository = roomRepository;
+
     }
 
     public Reservation createReservation(Reservation reservation){
+        if (!clientRepository.existsClient(reservation.getIdClient())){
+            throw new ClientNotFoundException("Client with id " + reservation.getIdClient() + " not found.");
+        }
+        if (!roomRepository.roomExists(reservation.getRoomNumber())){
+                    throw new RoomNotFoundException("Room with number " + reservation.getRoomNumber() + " not found.");
+        }
         return reservationRepository.createReservation(reservation);
     }
 
@@ -38,6 +48,13 @@ public class ReservationService {
         return reservationRepository.readReservationByClientId(idClient);
     }
 
+    public Optional<Reservation> readReservationByReservationId(Integer idReservation){
+        if (!reservationRepository.existsReservation(idReservation)){
+            throw new ReservationNotFoundException("Reservation with id " + idReservation + " not found.");
+        }
+        return Optional.of(reservationRepository.readReservationByReservationId(idReservation));
+    }
+
     public void deleteReservation(Integer idReservation){
         if (!reservationRepository.existsReservation(idReservation)){
             throw new ReservationNotFoundException("Reservation with id " + idReservation+ " not found.");
@@ -45,10 +62,11 @@ public class ReservationService {
         reservationRepository.deleteReservation(idReservation);
     }
 
-    public Optional<Reservation> updateReservation(Reservation reservation, Integer idClient){
+    public Optional<Reservation> updateReservation(Reservation reservation, Integer idReservation){
         if (!reservationRepository.existsReservation(reservation.getIdReservation())){
             throw new ReservationNotFoundException("Reservation with id " + reservation.getIdReservation() + " not found.");
         }
-        return Optional.of(reservationRepository.updateReservation(reservation,idClient));
+        return Optional.of(reservationRepository.updateReservation(reservation,idReservation));
     }
+
 }
