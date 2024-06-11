48521bb85e41e1e9e4a98c220243ed91e0712bb6
roberts <gabrielrobertssoares.com>
2024-06-11 16:27:24 -0300

    metodo para buscar reserva pelo id e obter ultimo id inserido no banco de dados

--- src/main/java/br/com/ada/reservala/repository/ReservationRepository.java
+++ src/main/java/br/com/ada/reservala/repository/ReservationRepository.java
@@ -1,6 +1,7 @@
 package br.com.ada.reservala.repository;
 
 import br.com.ada.reservala.domain.Reservation;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
@@ -11,15 +12,16 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReservationRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
-    private final String createSQL = "insert into reservation(id, idRoom, idClient, checkIn, checkOut) values (?, ?, ?, ?, ?)";
+    private final String createSQL = "insert into reservation(id, idClient, roomNumber, checkIn, checkOut) values (?, ?, ?, ?, ?)";
     private final String readSQL = "select * from reservation";
-    private final String updateSQL = "update reservation set  idRoom = ?, checkIn = ?, checkOut = ? where id = ?";
+    private final String updateSQL = "update reservation set  roomNumber = ?, checkIn = ?, checkOut = ? where id = ?";
     private final String deleteSQL = "delete from reservation where id = ?";
 
     public ReservationRepository(JdbcTemplate jdbcTemplate) {
@@ -27,14 +29,21 @@ public class ReservationRepository {
     }
 
     public Reservation createReservation(Reservation reservation){
+        int id;
+        if (getLastInsertedId().isEmpty()){
+            id = 1;
+        } else {
+            id = getLastInsertedId().get() + 1;
+        }
         jdbcTemplate.update(
                 createSQL,
-                reservation.getIdReservation(),
+                id,
                 reservation.getIdClient(),
                 reservation.getRoomNumber(),
                 reservation.getCheckIn(),
                 reservation.getCheckOut()
         );
+        reservation.setIdReservation(id);
         return reservation;
     }
 
@@ -71,13 +80,27 @@ public class ReservationRepository {
         return jdbcTemplate.query(psc,rowMapper);
     }
 
-    public Reservation updateReservation(Reservation reservation, Integer id){
+    public Reservation readReservationByReservationId(int idReservation){
+    RowMapper<Reservation> rowMapper = ((rs, rowNum) -> new Reservation(
+            rs.getInt("id"),
+            rs.getInt("idClient"),
+            rs.getInt("roomNumber"),
+            rs.getDate("checkIn").toLocalDate(),
+            rs.getDate("checkOut").toLocalDate()
+    ));
+
+    String sql = "select * from reservation where id = ?";
+
+    return jdbcTemplate.queryForObject(sql, rowMapper, idReservation);
+}
+
+    public Reservation updateReservation(Reservation reservation, Integer idReservation){
         jdbcTemplate.update(
                 updateSQL,
                 reservation.getRoomNumber(),
                 reservation.getCheckIn(),
                 reservation.getCheckOut(),
-                id
+                idReservation
         );
         return reservation;
     }
@@ -90,6 +113,15 @@ public class ReservationRepository {
 
     public LocalDate getCheckouDate(int roomNumber){
         return jdbcTemplate.queryForObject("select checkOut from reservation where roomNumber = ? order by checkOut desc limit 1", LocalDate.class,roomNumber);
+    }
+
+    public Optional<Integer> getLastInsertedId(){
+    try {
+        Integer lastId = jdbcTemplate.queryForObject("SELECT max(id) FROM reservation", Integer.class);
+            return Optional.ofNullable(lastId);
+                } catch (EmptyResultDataAccessException e) {
+                    return Optional.empty();
+        }
     }
 
     public Boolean existsReservation(int idReservation){
