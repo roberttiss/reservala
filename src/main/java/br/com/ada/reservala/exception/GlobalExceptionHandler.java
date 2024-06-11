2c1025b4a707d6f4cf1f33a12d41433a1f049f1c
roberts <gabrielrobertssoares.com>
2024-06-10 22:24:42 -0300

    adição de metodos de tratamento de exceções

--- src/main/java/br/com/ada/reservala/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/ada/reservala/exception/GlobalExceptionHandler.java
@@ -2,8 +2,12 @@ package br.com.ada.reservala.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -15,6 +19,24 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ClientNotFoundException.class)
     public ResponseEntity<String> handleClientNotFoundException(ClientNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IdAlreadyExistsException.class)
+    public ResponseEntity<String> handleIdAlreadyExistsException(IdAlreadyExistsException ex){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handledMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        String mesage = ex.getBindingResult().getAllErrors().stream()
+                .map(ObjectError::getDefaultMessage)
+                .collect(Collectors.joining("\n"));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mesage);
+    }
+
+    @ExceptionHandler(ReservationNotFoundException.class)
+    public ResponseEntity<String> handleReservationNotFoundException(ClientNotFoundException ex){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
     }
 }
